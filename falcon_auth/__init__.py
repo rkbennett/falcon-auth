c58d029f24abf08dd5559ffe158d9ee0fb7bc6d6
rkbennett <44292326+rkbennett@users.noreply.github.com>
2024-12-24 10:59:42 -0600

    Update __init__.py
    Adds async backend imports to module

--- falcon_auth/__init__.py
+++ falcon_auth/__init__.py
@@ -1,5 +1,17 @@
 # coding=utf-8
 
-from .backends import TokenAuthBackend, BasicAuthBackend, \
-    JWTAuthBackend, NoneAuthBackend, MultiAuthBackend, HawkAuthBackend
+from .backends import (
+    TokenAuthBackend, 
+    BasicAuthBackend,
+    JWTAuthBackend, 
+    NoneAuthBackend, 
+    MultiAuthBackend, 
+    HawkAuthBackend,
+    TokenAuthBackend_async, 
+    BasicAuthBackend_async,
+    JWTAuthBackend_async, 
+    NoneAuthBackend_async, 
+    MultiAuthBackend_async, 
+    HawkAuthBackend_async
+)
 from .middleware import FalconAuthMiddleware, FalconAuthMiddleware_async
